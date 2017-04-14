437fff7278a15de2a6ea7904a9dc921b06a928e2
AndreaLazarevic <andrea.lazarevic1996@gmail.com>
2017-04-14 16:27:51 +0200

    Napisana JavaDoc dokumentacija za klasu Twitter i njene metode.

--- Twitter/src/com/twitter/Twitter.java
+++ Twitter/src/com/twitter/Twitter.java
@@ -3,14 +3,30 @@ import java.util.LinkedList;
 
 import com.twitter.poruka.TwitterPoruka;
 
+/**
+ * Klasa Twitter u kojoj su implementirane metode za rad sa twitter porukama
+ * @author Andrea Lazarevic
+ *
+ */
 public class Twitter { 
-	
+	/**
+	 * lista objekata klase TwitterPoruka
+	 */
 	private LinkedList<TwitterPoruka> poruke = new LinkedList<TwitterPoruka>(); 
 	
+	/**
+	 * Vraca sve poruke koje se nalaze u listi.
+	 * @return listu poruke
+	 */
 	public LinkedList<TwitterPoruka> vratiSvePoruke(){
 		return poruke;
 	}
 	
+	/**
+	 * Unosi poruku na kraj liste
+	 * @param korisnik nickname twitter korisnika
+	 * @param poruka sadrzaj poruke koju salje korisnik
+	 */
 	public void unesi(String korisnik, String poruka) { 
 		//Pravi se nova poruka i puni podacima
 		TwitterPoruka tp = new TwitterPoruka(); 
@@ -22,6 +38,20 @@ public class Twitter {
 		poruke.addLast(tp); 
 	} 
 	
+	/**
+	 * Pronalazi i vraca poruke koje sadrze zadati tag u vidu niza objekata TwitterPoruka
+	 * @param maxBroj maksimalan broj poruka koji se moze upisati u niz
+	 * @param tag zadati tag
+	 * 
+	 * @throws java.lang.RuntimeException ako je unet tag koji je: 
+	 * <ul>
+	 * <li>null</li>
+	 * <li>prazan String</li>
+	 * </ul>
+	 * 
+	 * @return rezultat u vidu niza objekata TwitterPoruka
+	 * 
+	 */
 	public TwitterPoruka[] vratiPoruke(int maxBroj, String tag) { 
 		if(tag == null || tag.isEmpty()) 
 			throw new RuntimeException("Morate uneti tag");
